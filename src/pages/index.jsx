6248c3c5553a4cbeea6f15cd080d61ed156d2cdf
xinyichen03 <xinyichen03@163.com>
2024-06-15 22:08:21 +0100

    fix build

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -248,7 +248,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Hi, I'm Xinyi Chen
+            Hi, I’m Xinyi Chen
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Xinyi, an arts enthusiast now driving into the world of technology. I harness technology for impactful solutions in arts, culture, and media.
